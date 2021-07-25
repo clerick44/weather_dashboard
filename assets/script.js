b833f02cecf168d8e215d703be8c69d278b68e08
clerick44 <clerick44@hotmail.com>
2021-07-25 14:14:14 -0500

    api fetches complete, forecast layout complete, forecast display started in JS

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+  $("#currentDate").html(moment().format("ddd, MMM Do"));
+
   const searchBoxEl = document.querySelector("#searchBox");
   const searchButtonEl = document.querySelector("#searchButton");
   const locationEl = document.querySelector("#location");
@@ -12,20 +14,69 @@ $(document).ready(function () {
 
   searchButtonEl.addEventListener("click", fetchWeather);
 
-  var todayDate;
-
   function fetchWeather() {
-    todayDate = moment().format("ddd, MMM Do");
-
+    const degF = "&#8457";
+    var lat;
+    var lon;
     var locationVal = searchBox.value;
 
-    var fetchUrl =
+    var firstFetchUrl =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
       locationVal +
       "&units=imperial&appid=04540eb612d1334a8fb06879d6710e36";
-    console.log(fetchUrl);
-    fetch(fetchUrl)
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+    var secondFetchUrl =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&exclude=minutely,hourly,alerts&appid=04540eb612d1334a8fb06879d6710e36";
+
+    console.log(firstFetchUrl);
+    fetch(firstFetchUrl)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        $("#location").html(data.name);
+        $("#currentIcon").append(
+          $("<img>", {
+            id: "currentConditionsIcon",
+            src:
+              "http://openweathermap.org/img/wn/" +
+              data.weather[0].icon +
+              "@2x.png",
+          })
+        );
+        $("#currentTemp").html(Math.floor(data.main.temp) + degF);
+        $("#currentHumidity").html(data.main.humidity + "%");
+        $("#currentWind").html(Math.round(data.wind.speed) + " mph");
+
+        lat = data.coord.lat;
+        lon = data.coord.lon;
+        var secondFetchUrl =
+          "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+          lat +
+          "&lon=" +
+          lon +
+          "&exclude=minutely,hourly,alerts&appid=04540eb612d1334a8fb06879d6710e36";
+        console.log(secondFetchUrl);
+        fetch(secondFetchUrl)
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            $("#high").html(
+              Math.floor(((data.daily[0].temp.max - 273) * 9) / 5 + 32) + degF
+            );
+            $("#low").html(
+              Math.floor(((data.daily[0].temp.min - 273) * 9) / 5 + 32) + degF
+            );
+            $("#currentUvIndex").html(data.current.uvi);
+
+            for (let i = 1; i < data.daily.length; i++) {}
+          });
+      });
+
+    // .catch((data) {});
   }
 });
