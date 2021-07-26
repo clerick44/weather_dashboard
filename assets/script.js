94498f61af71bfb443dd4184a949502704608ead
clerick44 <clerick44@hotmail.com>
2021-07-26 15:24:49 -0500

    finished laout, js functioning needs bug testing and comments

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,27 @@
+const degF = "&#8457";
+
+var searchHistory = [];
+
 $(document).ready(function () {
+  loadHistory();
+  populateHistory();
+
+  $(".forecast").hide();
   $("#currentDate").html(moment().format("ddd, MMM Do"));
 
-  const searchBoxEl = document.querySelector("#searchBox");
-  const searchButtonEl = document.querySelector("#searchButton");
-  const locationEl = document.querySelector("#location");
-  const currentIconEl = document.querySelector("#currentIcon");
-  const currentDateEl = document.querySelector("#currentDate");
-  const currentTempEl = document.querySelector("#currentTemp");
-  const currentHighLowEl = document.querySelector("#currentHighLow");
-  const currentHumidityEl = document.querySelector("#currentHumidity");
-  const currentWindEl = document.querySelector("#currentWind");
-  const currentUvIndexEl = document.querySelector("#currentUvIndex");
+  $("#searchButton").click(function () {
+    fetchWeather(searchBox.value);
+  });
 
-  searchButtonEl.addEventListener("click", fetchWeather);
-
-  function fetchWeather() {
-    const degF = "&#8457";
+  function fetchWeather(event) {
+    console.log(event);
+    // console.log(data);
     var lat;
     var lon;
-    var locationVal = searchBox.value;
+    // var locationVal = searchBox.value;
+    var locationVal = event;
+    // updateHistory(locationVal);
+    populateHistory();
 
     var firstFetchUrl =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -31,52 +34,176 @@ $(document).ready(function () {
       lon +
       "&exclude=minutely,hourly,alerts&appid=04540eb612d1334a8fb06879d6710e36";
 
-    console.log(firstFetchUrl);
     fetch(firstFetchUrl)
       .then((response) => {
+        if (!response.ok) {
+          alert("Something has gone wrong. Error " + response.status);
+          return response.status;
+        }
         return response.json();
       })
       .then((data) => {
         $("#location").html(data.name);
-        $("#currentIcon").append(
-          $("<img>", {
-            id: "currentConditionsIcon",
-            src:
-              "http://openweathermap.org/img/wn/" +
-              data.weather[0].icon +
-              "@2x.png",
-          })
-        );
-        $("#currentTemp").html(Math.floor(data.main.temp) + degF);
-        $("#currentHumidity").html(data.main.humidity + "%");
-        $("#currentWind").html(Math.round(data.wind.speed) + " mph");
-
+        updateHistory(data.name);
         lat = data.coord.lat;
         lon = data.coord.lon;
+
         var secondFetchUrl =
           "https://api.openweathermap.org/data/2.5/onecall?lat=" +
           lat +
           "&lon=" +
           lon +
           "&exclude=minutely,hourly,alerts&appid=04540eb612d1334a8fb06879d6710e36";
-        console.log(secondFetchUrl);
+
         fetch(secondFetchUrl)
           .then((response) => {
+            if (!response.ok) {
+              alert("Something has gone wrong. Error " + response.status);
+              return response.status;
+            }
             return response.json();
           })
           .then((data) => {
-            $("#high").html(
-              Math.floor(((data.daily[0].temp.max - 273) * 9) / 5 + 32) + degF
-            );
-            $("#low").html(
-              Math.floor(((data.daily[0].temp.min - 273) * 9) / 5 + 32) + degF
-            );
-            $("#currentUvIndex").html(data.current.uvi);
-
-            for (let i = 1; i < data.daily.length; i++) {}
+            displayCurrentWeather(data);
+            displayForecast(data);
+          })
+          .catch((error) => {
+            console.error(error);
           });
+      })
+      .catch((error) => {
+        console.error(error);
       });
 
-    // .catch((data) {});
+    // .catch(data) {
+    return;
+  }
+
+  function displayCurrentWeather(data) {
+    $("#currentConditionsIcon").remove();
+    $("#currentIcon").append(
+      $("<img>", {
+        id: "currentConditionsIcon",
+        src:
+          "http://openweathermap.org/img/wn/" +
+          data.current.weather[0].icon +
+          "@2x.png",
+      })
+    );
+    $("#currentTemp").html(
+      Math.floor(((data.current.temp - 273) * 9) / 5 + 32) + degF
+    );
+    $("#currentHumidity").html(data.current.humidity + "%");
+    $("#currentWind").html(Math.round(data.current.wind_speed) + " mph");
+
+    $("#high").html(
+      Math.floor(((data.daily[0].temp.max - 273) * 9) / 5 + 32) + degF
+    );
+    $("#low").html(
+      Math.floor(((data.daily[0].temp.min - 273) * 9) / 5 + 32) + degF
+    );
+    $("#currentUvIndex").html(Math.floor(data.current.uvi));
+    var uvi = Math.floor(data.current.uvi);
+
+    if (uvi < 3) {
+      $("#currentUvIndex").css("background-color", "green");
+    } else if (uvi >= 3 && uvi < 6) {
+      $("#currentUvIndex").css("background-color", "yellow");
+    } else if (uvi >= 6 && uvi < 8) {
+      $("#currentUvIndex").css("background-color", "orange");
+    } else if (uvi >= 8 && uvi < 11) {
+      $("#currentUvIndex").css("background-color", "red");
+    } else {
+      $("#currentUvIndex").css("background-color", "purple");
+    }
+    return;
+  }
+
+  function displayForecast(data) {
+    var forecast = $("#forecastContainer").children();
+
+    $(".forecastDateEl").remove();
+    $(".forecastIconEl").remove();
+    $(".forecastHighLowEl").remove();
+    $(".forecast").show();
+
+    forecast.each(function (i, val) {
+      var offset = i + 1;
+
+      var dateDiv = $(this).children(":nth-child(1)");
+      var iconDiv = $(this).children(":nth-child(2)");
+      var highLowDiv = $(this).children(":nth-child(3)");
+
+      var forecastDate = moment
+        .unix(data.daily[offset].dt)
+        .format("ddd, MMM Do");
+      var highTemp =
+        Math.floor(((data.daily[offset].temp.max - 273) * 9) / 5 + 32) + degF;
+      var lowTemp =
+        Math.floor(((data.daily[offset].temp.min - 273) * 9) / 5 + 32) + degF;
+
+      $(dateDiv).append(
+        "<span class='forecastDateEl'>" + forecastDate + "</span>"
+      );
+
+      $(iconDiv).append(
+        $("<img>", {
+          class: "forecastIconEl",
+          src:
+            "http://openweathermap.org/img/wn/" +
+            data.daily[offset].weather[0].icon +
+            "@2x.png",
+        })
+      );
+
+      $(highLowDiv).append(
+        $(
+          "<span class='forecastHighLowEl'>" +
+            highTemp +
+            " / " +
+            lowTemp +
+            "</span>"
+        )
+      );
+    });
+  }
+
+  function loadHistory() {
+    var storedSearchHistory = JSON.parse(
+      localStorage.getItem("weatherSearchHistory")
+    );
+    if (storedSearchHistory !== null) {
+      searchHistory = storedSearchHistory;
+    }
+    return;
+  }
+
+  function updateHistory(val) {
+    if (searchHistory.length > 5) {
+      searchHistory.shift();
+    }
+
+    if (searchHistory.indexOf(val) !== -1) {
+      return;
+    }
+
+    searchHistory.push(val);
+    localStorage.setItem("weatherSearchHistory", JSON.stringify(searchHistory));
+    return;
+  }
+
+  function populateHistory() {
+    $(".historyButton").remove();
+
+    $.each(searchHistory, function (i, val) {
+      $("#searchHistory").append(
+        "<div class='historyButton'>" + searchHistory[i] + "</div>"
+      );
+    });
+    $(".historyButton").click(function () {
+      fetchWeather($(this).text());
+    });
+
+    return;
   }
 });
